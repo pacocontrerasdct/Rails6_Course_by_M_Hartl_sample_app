039151e43cc6f0c0bd72ae97b0e46fbd2dc36f65
pacocontrerasdct <pacocontrerasdct@gmail.com>
2021-05-11 11:03:19 +0000

    add password tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,4 +62,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?, "Password can not be blank"
+  end
+  
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?, "Password should have 6 characters at least"
+  end
 end
