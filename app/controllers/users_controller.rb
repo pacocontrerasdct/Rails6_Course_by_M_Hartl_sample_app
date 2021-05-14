c9a773ac6fb30039d7bc527134cd07c66a7d08bd
pacocontrerasdct <pacocontrerasdct@gmail.com>
2021-05-14 17:02:01 +0000

    add tests for signup and finish the controller for success request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
   
-  def new
-    
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    # debugger
+    @user = User.new
+  end
+  
+  def create
+    # debugger
+    @user = User.new(user_params)
+    
+    if @user.save
+      # Handle successful request
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.required(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+    end
 end
